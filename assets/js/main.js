19b5e56cba70048b5ce5497c30adce15a8e9e4a4
truonglongnguyen <nguyentruonglongblog@gmail.com>
2020-02-19 05:46:06 +0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -216,7 +216,7 @@ jQuery(document).ready(function($){
     
         /*-- Headertyping effect activatioin start--*/
         var typed = new Typed('.htitle', {
-          strings: ["TRUONG <span>LONG</span>",'SOFTWARE <span>ENGINEER</span>','WEBSITE <span>BACKEND</span>'],
+          strings: ['TRUONG <span>LONG</span>', 'WEB <span>BACKEND</span>', 'SOFTWARE <span>ENGINEER</span>'],
           typeSpeed: 50,
           backSpeed: 60,
           smartBackspace: false,
