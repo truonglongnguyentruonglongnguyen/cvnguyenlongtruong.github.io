d6e19e66602a1d49d5986a202cdfb95a81f609a3
truonglongnguyen <nguyentruonglongblog@gmail.com>
2020-06-29 22:45:17 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -317,33 +317,6 @@ jQuery(document).ready(function($){
 
         /*-- Nguyen Truong Long --*/
 
-
-            // $(window).scroll(function() {
-            //     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-            //     $(".fade-scroll").each(function() {
-            //       /* Check the location of each desired element */
-            //       var objectBottom = $(this).offset().top + $(this).outerHeight();
-
-            //       var objectTop = $(this).offset().top;
-                  
-            //       /* If the element is completely within bounds of the window, fade it in */
-            //       if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-            //         if ($(this).css("opacity")==0) {
-            //             $(this).animate({ left: "0px", opacity: "1" }, 500);
-            //         }
-            //       } 
-            //       if(objectTop >= windowBottom) {   //object goes out of view (scrolling up)
-            //         if ($(this).css("opacity")==1) {
-            //           if($(document).width() >= 576) {
-            //             $(this).animate({ opacity: "0", left: "50px" }, 500);
-            //           } else {
-            //             $(this).animate({ opacity: "0", left: "20px" }, 500);
-            //           }
-            //         }
-            //       }
-            //     });
-            //   }).scroll(); //invoke scroll-handler on page-load
-
     
     jQuery(window).load(function(){
       
